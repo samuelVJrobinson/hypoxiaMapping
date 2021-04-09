d6539849b3d9078e6e5a4a9728a8e44ed022f3e1
Samuel Robinson <samuel.vj.robinson@gmail.com>
2021-04-09 14:00:04 -0600

    Updated prelim

--- helperFunctions.R
+++ helperFunctions.R
@@ -38,3 +38,24 @@ geom2cols <- function(d,x=lon,y=lat,removeGeom=TRUE,epsg=NA){
   if(removeGeom) d <- st_drop_geometry(d) #Drop geometry
   return(d)
 }
+
+
+#Basic bivariate correlation plots (used in pairs() function)
+panel.hist <- function(x, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col = "grey", ...)
+}
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- cor(x, y)
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * abs(r),col=ifelse(r>0,'black','red'))
+}
+
--- prelim.R
+++ prelim.R
@@ -29,26 +29,28 @@ wDat <- read.csv('./data/sample_waterData.csv') %>% mutate(Date=as.Date(Date,'%Y
 
 #Get locations from wDat to join onto spatial data
 locIndex <- wDat %>% select(YEID,Date,doy,geometry) %>% distinct() %>%
-  geom2cols(E,N,removeGeom=FALSE,epsg=3401) %>% 
-  mutate(sE=(E-mean(E))/1000,sN=(N-mean(N))/1000) #Center E/N and convert to km
+  geom2cols(E,N,removeGeom=FALSE,epsg=3401) %>% #Louisiana offshore 
+  mutate(sE=(E-mean(E))/1000,sN=(N-mean(N))/1000) %>% #Center E/N and convert to km
+  st_transform(4326)
   
 #Spectral data
 sDat <- read.csv('./data/sample_spectralData.csv') %>% 
   left_join(locIndex,by='YEID') %>% #Join in spatial info
+  select(-Date,-doy) %>% 
   st_as_sf() %>% 
-  mutate(date_img=as.Date(date_img,'%Y-%m-%d')) %>% 
+  mutate(date_img=as.Date(date_img,'%Y-%m-%d'),doy=as.numeric(format(date_img,'%j'))) %>% 
   mutate(nflh=ifelse(nflh<0,0,ifelse(nflh>1,1,nflh))) %>% #Limits nflh to between 0 and 1
   mutate(poc=ifelse(nflh<0,1,poc)) #Changes negative poc to 1 (really low)
 
 #Not sure what the other channels are, so removing them for now, and stripping out data-less days
-sDat2 <- sDat %>% select(YEID:poc,E:geometry) %>% 
+sDat2 <- sDat %>% select(YEID,date_img,doy,chlor_a:poc,E:geometry) %>% 
   mutate(noData=is.na(chlor_a)&is.na(nflh)&is.na(poc)) %>% #Strip missing data
   group_by(date_img) %>% mutate(n=n(),nNoDat=sum(noData)) %>% #All have 205 locations
   filter(nNoDat<(n*0.5)) %>% #Remove days where >50% of data are missing
   select(-noData:-nNoDat) %>% ungroup() 
 
 #Base map
-spDomain <- wDat %>% st_union() %>% st_convex_hull() %>% st_buffer(dist=1) %>% st_bbox() #Spatial domain
+spDomain <- wDat %>% st_union() %>% st_convex_hull() %>% st_buffer(dist=0.25) %>% st_bbox() #Spatial domain
 names(spDomain) <- c('left','bottom','right','top')
 basemap <- get_map(location=spDomain) #Get from Google Earth
 
@@ -59,7 +61,6 @@ surfWDat <- wDat %>% group_by(YEID) %>% mutate(depthOrd=order(Depth)) %>% ungrou
 #choose only bottom water
 bottomWDat <- wDat %>% group_by(YEID) %>% mutate(depthOrd=order(Depth,decreasing=TRUE)) %>% ungroup() %>% 
   filter(depthOrd==1) %>% select(-depthOrd)
-
 
 # Take a look at water data ---------------------
 
@@ -95,25 +96,6 @@ p <- surfWDat %>% st_drop_geometry() %>% pivot_longer(c(DO:Salin)) %>%
   facet_wrap(~name,ncol=1)
 ggsave('./figures/prelimFigs/wDat_overall_ts.png',p,width=8,height=8)
 
-#Basic bivariate correlation plots
-panel.hist <- function(x, ...){
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(usr[1:2], 0, 1.5) )
-  h <- hist(x, plot = FALSE)
-  breaks <- h$breaks; nB <- length(breaks)
-  y <- h$counts; y <- y/max(y)
-  rect(breaks[-nB], 0, breaks[-1], y, col = "grey", ...)
-}
-panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...){
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(0, 1, 0, 1))
-  r <- cor(x, y)
-  txt <- format(c(r, 0.123456789), digits = digits)[1]
-  txt <- paste0(prefix, txt)
-  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
-  text(0.5, 0.5, txt, cex = cex.cor * abs(r),col=ifelse(r>0,'black','red'))
-}
-
 png(file = './figures/prelimFigs/wDat_overall_cor.png',width=6,height=6,units='in',res=150)
 surfWDat %>% st_drop_geometry() %>% select(DO:Salin) %>% pairs(upper.panel=panel.cor,diag.panel=panel.hist)
 dev.off()
@@ -128,16 +110,12 @@ p <- wDat %>% st_drop_geometry() %>%
   scale_y_reverse()+labs(y='Depth',x='Measurement')
 ggsave('./figures/prelimFigs/wDat_depth_overall.png',p,width=8,height=10)
 
+#Same thing, but with proportion depth
 wDat %>% st_drop_geometry() %>% 
   pivot_longer(c(DO:Salin)) %>% mutate(fDate=dateCut(Date,6)) %>% 
-  ggplot()+  geom_point(aes(x=value,y=Depth/Depth_dem),alpha=0.3)+ #Uses proportional depth
+  ggplot()+  geom_point(aes(x=value,y=propDepth),alpha=0.3)+ #Uses proportional depth
   facet_grid(fDate~name,scales='free_x')+
   scale_y_reverse()+labs(y='Proportion Depth',x='Measurement')
-
-wDat %>% group_by(YEID) %>% mutate(mDepth=max(Depth,Depth_dem)) %>% data.frame()
-
-wDat %>% group_by(YEID) %>% slice(1)
-
 
 # Take a look at spectral data ----------------------------
 sDat
@@ -148,7 +126,6 @@ p <- sDat %>% select(YEID:poc) %>% pivot_longer(c(chlor_a:poc)) %>% filter(!is.n
   geom_vline(xintercept=range(wDat$Date),col='red',linetype='dashed') #Range of water data
 ggsave('./figures/prelimFigs/sDat_overall_ts.png',p,width=8,height=8)
 
-
 #Make animation of spectral data across time range
 gen_anim <- function() { #Generate set of frames
   sDat3 <- sDat2 %>% mutate(across(chlor_a:poc,~as.vector(scale(.x)))) #%>% pivot_longer(chlor_a:poc)
@@ -156,7 +133,7 @@ gen_anim <- function() { #Generate set of frames
     s <- sDat3 %>%  filter(date_img == tau) #subset data to time step tau
       # %>% filter(!is.na(value))
     f <- function(dat,channel,lims){ #Function to make standard figure
-      ggmap(basemap) + geom_sf(data=s,aes(geometry=geometry,colour = {{channel}},size={{channel}}),inherit.aes = FALSE) +
+      ggmap(basemap) + geom_sf(data=dat,aes(geometry=geometry,colour = {{channel}},size={{channel}}),inherit.aes = FALSE) +
         guides(size=FALSE)+
         scale_size(limits=lims)+
         # scale_colour_gradient(limits=lims)
@@ -204,7 +181,7 @@ p2 <- sDat2 %>% mutate(lat=st_coordinates(.)[,2]) %>%
   scale_fill_distiller(type='div',palette = "YlOrBr",na.value=NA)+
   theme(panel.background = element_rect(fill = 'grey'))+
   labs(x='Latitude',y='Date')+theme(legend.position = 'bottom')
-p <- ggarrange(p1,p2,ncol=2,common.legend=TRUE,legend='right')
+(p <- ggarrange(p1,p2,ncol=2,common.legend=TRUE,legend='right'))
 ggsave('./figures/prelimFigs/sDat_hovmoller.png',p,width=12,height=8)
 
 # sDat2 %>% mutate(lon=st_coordinates(.)[,1]) %>% mutate(lon=cut(lon,breaks=20)) %>% st_drop_geometry() %>% 
@@ -213,10 +190,15 @@ ggsave('./figures/prelimFigs/sDat_hovmoller.png',p,width=12,height=8)
 
 #Pretty low correlation between classes. Do these actually mean the same thing? Ask YL or LN how these are typically used.
 png(file = './figures/prelimFigs/sDat_overall_cor.png',width=6,height=6,units='in',res=150)
-sDat2 %>% select(chlor_a:poc) %>% na.omit() %>% st_drop_geometry() %>% pairs(upper.panel=panel.cor,diag.panel=panel.hist)
+sDat %>% select(chlor_a:poc) %>% na.omit() %>% st_drop_geometry() %>% pairs(upper.panel=panel.cor,diag.panel=panel.hist)
 dev.off()
 
-# Fit model of chlor_a ----------------------------------------------------
+#Almost no chlor_a measurements on days when DO measurments were taken. Need some way of filling in the gaps
+bottomWDat %>% left_join(mutate(st_drop_geometry(sDat),doy=doy),by=c('YEID','doy')) %>% 
+  filter(!is.na(chlor_a)) %>% 
+  ggplot(aes(chlor_a,DO))+geom_point()
+
+# Fit GAM of chlor_a ----------------------------------------------------
 library(mgcv)
 
 #Using entire dataset. Fit is somewhat better if using reduced dataset (sDat2), but going with this for now.
@@ -238,7 +220,7 @@ par(mfrow=c(2,2)); gam.check(chlorMod); abline(0,1,col='red'); par(mfrow=c(1,1))
 plot(chlorMod,scheme=3,too.far=0.1)
 chlorDat$resid <- resid(chlorMod) #Residuals
 
-#Get predictions
+#Prediction grid
 predChlor <- with(chlorDat,expand.grid(doy=seq(min(doy),max(doy),length.out=9),loc=unique(paste(sE,sN,sep='_')))) %>% 
   separate(loc,c('sE','sN'),sep='_',convert=TRUE) %>% 
   mutate(pred=predict(chlorMod,newdata=.),se=predict(chlorMod,newdata=.,se.fit=TRUE)$se.fit) %>% 
@@ -256,8 +238,13 @@ p2 <- predChlor %>% ggplot()+geom_point(aes(x=sE,y=sN,col=se))+
   labs(x='E',y='N',col='SE')+
   theme(legend.position='bottom')
 
-p <- ggarrange(p1,p2,nrow=2)
+(p <- ggarrange(p1,p2,nrow=2))
 ggsave('./figures/prelimFigs/chlorMod.png',p,width=11,height=6)
+
+#Plot of residuals over space for 5 time slices. Looks like both models are doing poorly at the same places
+chlorDat %>% mutate(cDate=dateCut(date_img,12)) %>%
+  ggplot()+geom_sf(aes(geometry=geometry,col=resid,size=abs(resid)),alpha=0.5)+
+  facet_wrap(~cDate)+ scale_colour_distiller(type='div',palette = "RdBu")
 
 # #Try larger tensor product (10x10x10).  What has 500 more coefficients gotten us?
 # chlorMod2 <- gam(logChlor~te(sN,sE,doy,k=10),data=chlorDat)  #Better AIC, but residual checks are about the same.
@@ -296,30 +283,64 @@ predChlor <- with(chlorDat,expand.grid(YEID=unique(YEID),doy=min(doy):max(doy)))
   mutate(pred=predict(chlorMod,newdata=.),se=predict(chlorMod,newdata=.,se.fit=TRUE)$se.fit) %>% 
   mutate(date=as.Date(paste('2020',round(doy),sep='-'),format='%Y-%j'))
 
-lags <- 0:60 #Try 0 to 60 day lags
+lags <- -40:40 #Try -40 to 40 day lags (negative = days after, positive = days before)
 modList <- lapply(lags,function(i){
+  #Copies of water data
   sWatTemp <- surfWDat
   bWatTemp <- bottomWDat 
   chooseThis <- predChlor$YEID==sWatTemp$YEID & predChlor$doy==(sWatTemp$doy-i) #Location of lagged chlor predictions
   sWatTemp$lagChlor <- predChlor$pred[chooseThis]
+  sWatTemp$lagDay <- predChlor$doy[chooseThis]
   bWatTemp$lagChlor <- predChlor$pred[chooseThis]
+  bWatTemp$lagDay <- predChlor$doy[chooseThis]
   sMod <- lm(DO~lagChlor,data=sWatTemp)
   bMod <- lm(DO~lagChlor,data=bWatTemp)
   return(list(surface=sMod,bottom=bMod))
 })
 
-data.frame(lag=lags,surface=sapply(modList,function(i) sum(resid(i$surface)^2)),
+p1 <- data.frame(lag=lags,surface=sapply(modList,function(i) sum(resid(i$surface)^2)),
            bottom=sapply(modList,function(i) sum(resid(i$bottom)^2))) %>% 
   pivot_longer(surface:bottom) %>% 
-  ggplot()+geom_point(aes(x=lag,y=value))+
-  facet_wrap(~name)+
+  ggplot()+geom_line(aes(x=lag,y=value))+
+  geom_vline(xintercept = 0,linetype='dashed')+facet_wrap(~name)+
   labs(x='Time lag',y='Mean Squared Error')
 
-#Looks like surface water DO is more poorly predicted by chlor_a
-data.frame(lag=lags,surface=sapply(modList,function(i) summary(i$surface)$r.squared),
+p2 <- data.frame(lag=lags,surface=sapply(modList,function(i) summary(i$surface)$r.squared),
            bottom=sapply(modList,function(i) summary(i$bottom)$r.squared)) %>% 
   pivot_longer(surface:bottom) %>% 
-  ggplot()+geom_point(aes(x=lag,y=value))+
-  facet_wrap(~name)+
+  ggplot()+geom_line(aes(x=lag,y=value))+
+  geom_vline(xintercept = 0,linetype='dashed')+facet_wrap(~name)+
   labs(x='Time lag',y='R-squared')
+
+ggarrange(p1,p2,ncol=1)
+
+# NOTES:
+# Looks like surface water DO is more poorly predicted by chlor_a. However, lower MSE, so probably just less variable overall
+# Strangely, future chlor_a is a slightly better predictor than past chlor_a. What is going on here?
+# Hypotheses:
+# 1) Just a weird dataset, and won't show up in future years
+# 2) Some thing is actually causing a DO drop, followed by an increase in chlor_a
+
+#Take a look at model on day -11
+dayLag <- which.min(sapply(modList,function(i) sum(resid(i$bottom)^2)))
+bestMod <- modList[[dayLag]]$bottom #Bottom water model
+with(bestMod$model,plot(lagChlor,DO,xlab=paste0('Chlor_a at day ',lags[dayLag]),ylab='DO'))
+abline(bestMod)
+
+#
+rm(bWatTemp,sWatTemp)
+
+dayLag <- which.min(sapply(modList,function(i) sum(resid(i$bottom)^2)))
+bestMod <- modList[[dayLag]]$bottom #Bottom water model
+with(bestMod$model,plot(lagChlor,DO,xlab=paste0('Chlor_a at day ',lags[dayLag]),ylab='DO'))
+abline(bestMod)
+
+
+#Trying functional regression using lagged predictions
+fdat <- list(
+  
+)
+
+
+
 
