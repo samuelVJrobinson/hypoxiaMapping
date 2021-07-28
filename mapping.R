f1d43db0c51453c179878e2f0ee6b9a37c6ab393
Samuel Robinson <samuel.vj.robinson@gmail.com>
2021-07-28 09:52:19 -0600

    Files were wrong resolution
    - Tried functions to find edges between cells, but didn't work

--- mapping.R
+++ mapping.R
@@ -26,24 +26,116 @@ files <- data.frame(paths=dir(storage,recursive=TRUE,full.names = TRUE)) %>%
   mutate(combined=paste0(newfolder,'/',combined)) %>% 
   mutate(exists=file.exists(combined))
 
+# #Average between aqua and terra
 # a <- Sys.time()
 # for(i in 1:nrow(files)){
 #   if(!files$exists[i]){
 #     aquaDat <- brick(files$aqua[i])
 #     terraDat <- brick(files$terra[i])
-#     
+# 
 #     newDat <- lapply(1:dim(aquaDat)[3],function(j) calc(stack(aquaDat[[j]],terraDat[[j]]),fun=mean,na.rm=TRUE))
 #     newDat <- brick(newDat)
 #     names(newDat) <- names(aquaDat)
 #     writeRaster(newDat,filename=files$combined[i],format='GTiff')
 #     rm(newDat)
-#     gc()    
+#     gc()
 #   }
 #   print(paste0('Finished file ',i,' of ',nrow(files)))
 # }
 # b <- Sys.time()
-# b-a #Takes about an hour. Also causes problems because of background file storage within raster library
+# b-a #Takes about 10 mins
 
-brick(files$aqua[1])
+#Combined channels are missing their names, but this isn't that big of a deal. 
+cNames <- names(brick(files$aqua[1])) #Channel names
+datPath <- files$combined[1] #First combined stack
+# datPath <- files$aqua[1] #Aqua stack
 
-sDat <- brick(files$combined[1])
+sDat <- brick(datPath)
+names(sDat) <- cNames
+sDat2 <- rasterToPoints(sDat) #Convert to points
+nMissing <- apply(sDat2[,!grepl('(x|y)',colnames(sDat2))],1,function(x) sum(is.na(x))) #Proportion of missing values in each row (cell)
+sDat2 <- data.frame(sDat2[nMissing<5,]) #Keep points with 4 or more 
+
+sDat2 %>% 
+  filter(x>(-86.75),x<(-86.25),y<30.2,y>29.8) %>% #Weird sampling going on; way too many points per cell
+  ggplot(aes(x=x,y=y,fill=chlor_a))+
+  geom_raster()
+
+#No simple way to do this using raster. Resample using tidyverse syntax?
+
+# ext <- extent(c(-86.65,-86.35,29.8,30.1))
+# sDatSmall <- crop(sDat[[1]],ext)
+# 
+# plot(sDatSmall)
+# extract(sDatSmall,ext)
+# 
+# # convert to SPDF
+# gridDat <- as(sDatSmall,'SpatialPolygonsDataFrame')
+# spplot(gridDat)
+# 
+# sDatSmall2 <- aggregate(sDat,fact=c(10,9),fun=max)
+# 
+# plot(sDatSmall)
+
+l <- rep(c(1,2,5,3),c(3,4,2,6))
+l2 <- rep(c(3,NA,3.5,4,NA,5),c(5,2,3,5,5,5))
+
+blockSize <- function(x){ #Gets size of blocks in a vector of NAs/reals
+  xNA <- is.na(x)
+
+  x1 <- x[!xNA]-lag(x[!xNA],1)
+  x1[1] <- 1
+  x1 <- x1!=0
+  x2 <- as.numeric(!xNA)
+  x2[!xNA] <- cumsum(x1)
+  
+  x3 <- x2 - lag(x2,1)
+  x3[1] <- 1
+  x3 <- x3!=0
+  unname(table(cumsum(x3)))
+}
+
+debugonce(blockSize)
+blockSize(l)
+blockSize(l2)
+
+
+apply(as.matrix(sDat[[1]]),1,blockSize)
+# apply(as.matrix(sDat[[1]]),2,blockSize)
+
+blockGroup <- function(x){
+  xNA <- is.na(x)
+  x1 <- x[!xNA]-lag(x[!xNA],1)
+  x1[1] <- 1
+  x1[abs(x1)<.Machine$double.eps] <- 0
+  x1 <- x1!=0
+  cumsum(x1)
+  x[!xNA] <- cumsum(x1)
+  return(x)
+}
+# 
+# blockGroup(l)
+# blockGroup(l2)
+
+sDat3 <- sDat2 %>% 
+  arrange(x,y) %>% mutate(across(.cols=chlor_a:sst,~blockGroup(.x),.names = "{.col}_xgroup")) %>%
+  unite(col='x_group',contains("_xgroup"),sep='_') %>% mutate(x_group=as.numeric(factor(x_group))) %>%
+  arrange(y,x) %>% mutate(across(.cols=chlor_a:sst,~blockGroup(.x),.names = "{.col}_ygroup")) %>%
+  unite(col='y_group',contains("_ygroup"),sep='_') %>% mutate(y_group=as.numeric(factor(y_group))) %>%
+  unite(col='cell_group',contains("_group"),sep='_') %>% mutate(cell_group=as.numeric(factor(cell_group))) %>% 
+  group_by(cell_group) %>% summarize(across(everything(),~mean(.x)))
+
+sDat3 %>% filter(x>(-86.75),x<(-86.25),y<30.2,y>29.8) %>% #Weird sampling going on; way too many points per cell
+  ggplot(aes(x=x,y=y,col=chlor_a))+geom_point(size=0.5) 
+
+sDat2 %>% 
+  # filter(x>(-86.75),x<(-86.25),y<30.2,y>29.8) %>%
+  filter(x>(-86.4),x<(-86.3),y<30,y>29.9) %>%
+  arrange(y,x) %>% mutate(across(.cols=chlor_a:sst,~blockGroup(.x),.names = "{.col}_xgroup")) %>%
+  unite(col='x_group',contains("_xgroup"),sep='_') %>% mutate(x_group=as.numeric(factor(x_group))) %>%
+  arrange(x,y) %>% mutate(across(.cols=chlor_a:sst,~blockGroup(.x),.names = "{.col}_ygroup")) %>%
+  unite(col='y_group',contains("_ygroup"),sep='_') %>% mutate(y_group=as.numeric(factor(y_group))) %>%
+  ggplot(aes(y=y))+
+  # geom_text(aes(x=x,label=y_group),alpha=0.5,col='red')+
+  geom_text(aes(x=x+0.001,label=x_group),col='black')
+  # geom_point(aes(col=y_group),size=1.5) 
